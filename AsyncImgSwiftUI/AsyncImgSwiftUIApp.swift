52c57433ef8ccbf5ba4925eefaa05e9a205beb7e
nursah <nursahari@hotmail.com>
2021-12-26 16:29:52 +0300

    Initial Commit

---
+++ AsyncImgSwiftUI/AsyncImgSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImgSwiftUIApp.swift
+//  AsyncImgSwiftUI
+//
+//  Created by Nurşah on 26.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImgSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
